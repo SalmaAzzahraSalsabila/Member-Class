8560a706c6822e1774dbe2dab414417abd5abb4f
Salma Azzahra Salsabila <salma.2000@students.amikom.ac.id>
2019-04-13 08:43:04 +0700

    member class

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Object
+            Taxi Taxi = new Taxi();
+
+            //Properties
+            Taxi.DriverName = "Jono";
+            Taxi.OnDuty = true;
+            Taxi.NumPassenger = 10;
+
+            //methode
+            Taxi.TaxiInfo();
+            Taxi.PickUpPassenger();
+            Taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass {
+    class Taxi 
+    {
+
+        //properties 
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; } 
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("DriverName: {0}", DriverName);
+            Console.WriteLine("OnDuty: {0}", OnDuty ? "yes" : "no");
+            Console.WriteLine("NumPassenger: {0}\n", NumPassenger); 
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} Jono sedang menjeput penumpang ", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} Jono sedang mengantar penumpang \n", DriverName);
+        }
+    }
+}
